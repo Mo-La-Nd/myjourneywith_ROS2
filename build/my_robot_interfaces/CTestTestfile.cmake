0d14a31d4e2697c0cd5e2bcf5483f8b08e6f1750
Mo-La-Nd <landiaye_98@hotmail.com>
2024-02-14 13:30:41 +0100

    Topics, Services, and Actions templates

---
+++ build/my_robot_interfaces/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ml_nd/ROS2Level3_ws/my_robot_interfaces
+# Build directory: /home/ml_nd/ROS2Level3_ws/build/my_robot_interfaces
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("my_robot_interfaces__py")
